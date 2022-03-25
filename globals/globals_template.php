9d10bc3b37f01766edbf6e869c108b07eb5bfddb
BingPascual <bpascual@inmotiongroup.com>
2022-03-25 16:34:47 -0400

    Initial Commit

---
+++ globals/globals_template.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bing2
+ *
+ */
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+date_default_timezone_set('America/Detroit');
+define ("BASE_PATH","/" );
+define ("TEMP_DIR", BASE_PATH . "temp/");
+define ("GENERATED_FILES_DIR", BASE_PATH . "genearted/");
+
+define ("DEVICE_51D_DOWNLOAD", '');
+define ("DEVICE_51D_KEY", '');
+define ("DEVICE_51D_FILETYPE", '');
+define ("DEVICE_51D_FILENAME_PACKAGE", '');
+define ("DEVICE_51D_FILENAME_TARGET", '');
+define ("DEVICE_51D_BACKUP_DIR", '');
+
+define ("MAXMIND_KEY",'');
+define ("MAXMIND_UID",'');
+
+define ("DB_HOST",'');
+define ("DB_USER",'');
+define ("DB_PASS",'');
+define ("DB_NAME",'');
+
+$database_config['default'] = array(
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'database' => ''
+);
+
