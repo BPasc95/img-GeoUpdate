f935a34a29009ba05c0b1b00a85554439bdef7e7
BingPascual <bpascual@inmotiongroup.com>
2022-03-25 16:44:50 -0400

    Initial Commit

--- globals/globals_template.php
+++ globals/globals_template.php
@@ -4,12 +4,14 @@
  * User: Bing2
  *
  */
+
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 date_default_timezone_set('America/Detroit');
 define ("BASE_PATH","/" );
 define ("TEMP_DIR", BASE_PATH . "temp/");
 define ("GENERATED_FILES_DIR", BASE_PATH . "genearted/");
+
 
 define ("DEVICE_51D_DOWNLOAD", '');
 define ("DEVICE_51D_KEY", '');
@@ -33,3 +35,4 @@ $database_config['default'] = array(
     'database' => ''
 );
 
+include BASE_PATH . "globals/version.php";
